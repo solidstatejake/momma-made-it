176e7259da1fa556d1f062fec29e420bb3fd5103
ssj <jberg3@binghamton.edu>
2020-07-18 23:16:23 -0400

    Begin implementing / styling recipe feed.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe }            from '../../models/recipe.model';
+
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  selector    : 'app-recipe-list',
+  templateUrl : './recipe-list.component.html',
+  styleUrls   : [ './recipe-list.component.scss' ]
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Delicious tests. Mmmm...',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thestar.com%2Fcontent%2Fdam%2Fthestar%2Flife%2Ffood_wine%2F2014%2F04%2F04%2Fkavkaz_a_welcome_taste_of_azerbaijan_in_north_york%2Flavangi_walnutstuffed_cornish_hen_at_kavkaz.jpg&f=1&nofb=1'
+    ),
+    new Recipe(
+      'A Second Test Recipe',
+      'These tests are sooooo good!',
+      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.huffpost.com%2Fgen%2F1333558%2Fimages%2Fo-BEST-FOOD-BLOGGERS-facebook.jpg&f=1&nofb=1'
+    )
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() : void {
   }
 
 }
